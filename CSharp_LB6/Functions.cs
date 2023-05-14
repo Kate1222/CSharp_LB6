71c40c6f573e2dbeda1f8641ba96f0a4b61db74b
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-05-14 21:10:31 +0300

    Add file Funcitons.cs to repository

---
+++ CSharp_LB6/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace CSharp_LB6
+{
+    public class Functions
+    {
+        public string GetUserNameFromDialog()
+        {
+            var userNameDialog = new UserNameDialog();
+            userNameDialog.ShowDialog();
+            var userName = userNameDialog.userName;
+
+            return userName;
+        }
+        
+        public string GetUserName()
+        {
+            string userName;
+            
+            if (!File.Exists("username.json"))
+            {
+                userName = GetUserNameFromDialog();
+                var saveUserNameJsonFile = JsonSerializer.Serialize(userName);
+                File.WriteAllText("username.json", saveUserNameJsonFile);
+            }
+            else
+            {
+                var readUserNameJsonFile = File.ReadAllText("username.json");
+                userName = JsonSerializer.Deserialize<string>(readUserNameJsonFile);
+            }
+
+            return userName;
+        }
+    }
+}
