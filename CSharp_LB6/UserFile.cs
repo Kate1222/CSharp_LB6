467dfe55ec740ad134d3c61c8492ddd74a949ba3
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2023-05-15 15:18:15 +0300

    Revert "Revert "Realize dialog window where user can switch access to personal file""
    This reverts commit 5b144782abe98b2bef384ed9c192b610530f5c31.

--- CSharp_LB6/UserFile.cs
+++ CSharp_LB6/UserFile.cs
@@ -9,15 +9,5 @@ namespace CSharp_LB6
         public string path;
         public DateTime createDate;
         public bool isAvailable;
-
-        /*public UserFile(string name, string format, string fileWeight, string path, bool isAvailable)
-        {
-            this._name = name;
-            this._format = format;
-            this._fileWeight = fileWeight;
-            this._path = path;
-            this._isAvailable = isAvailable;
-        }*/
-        //createDate
     }
 }
